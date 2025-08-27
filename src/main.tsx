8a3b78de43c438e6f03d0a3eaeb431f22bb51daa
typrashant <ty.prashant.gmail.com>
2025-08-27 23:30:52 +0530

    updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './app';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
