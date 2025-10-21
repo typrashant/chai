dd00081b426add4720f04040800ab695ba5bca11
typrashant <ty.prashant.gmail.com>
2025-10-21 14:48:09 +0530

    21101448

---
+++ app/src/main/java/app/vercel/chai_nqnh/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.chai_nqnh.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
